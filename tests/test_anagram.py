6567d6579d64cfa598f6da44b7cc1c71a0a138bf
Mariyah Rohim <mariyah.rohim@bankofengland.co.uk>
2026-01-16 14:07:58 +0000

    fails tests

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -5,5 +5,11 @@ class SmokeTest(unittest.TestCase):
     def test_basic_true(self):
         self.assertTrue(is_anagram("listen","silent"))
 
+    def test_basic_true(self):
+        self.assertTrue(is_anagram("apple","pear"))
+
+    def test_basic_true(self):
+        self.assertTrue(is_anagram("lemon","melon"))
+
 if __name__ == "__main__":
     unittest.main()
